87eff08e012960ecca3c4c08c93ac120ae66aeef
EyasuTeshome <joshtesh91@mail.com>
2022-07-14 18:09:25 +0800

    I have fixed the comments given by my code reviewer (the required ones)

--- src/index.js
+++ src/index.js
@@ -1,39 +1,28 @@
 /* eslint-disable */
 import './style.css';
 
-function showTodo(todo) {
-    const list = document.querySelector('.list');
-    const newlist = document.createElement('li');
-    newlist.classList.add('newlist');
-    newlist.innerHTML = `
+
+const todoList = [{
+        index: '1',
+        description: 'Wake up at 6 AM'
+    },
+    {
+        index: '2',
+        description: 'Breakfast at 8 AM'
+    },
+    {
+        index: '3',
+        description: 'Go to Work'
+    },
+]
+
+
+const newlist = document.querySelector('.list1');
+
+newlist.innerHTML = todoList.map((todo) => `
+<li class="newlist1">
       <input id="${todo.index}" class="check" type="checkbox"/>
       <span class="text">${todo.description}</span>
       <button class="delete"><img src="https://e7.pngegg.com/pngimages/179/938/png-clipart-computer-icons-hamburger-button-dots-kebab-menu-text-rectangle-thumbnail.png"></button>
-    `;
-    list.append(newlist);
-}
-
-const todoList = [];
-let id = 1;
-
-function add(text) {
-    const todo = {
-        description: text,
-        completed: false,
-        index: id += 1,
-    };
-    todoList.push(todo);
-    showTodo(todo);
-}
-
-const form = document.querySelector('.main');
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const input = document.querySelector('.action');
-    const text = input.value.trim();
-    if (text !== '') {
-        add(text);
-        input.value = '';
-        input.focus();
-    }
-});
+      </li>
+    `);
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
+    mode: 'development',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
